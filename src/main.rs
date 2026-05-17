68140833fbd862b165ec59c3de8875fa78de1a4f
hakabol <habelcijuanu@gmail.com>
2026-05-17 12:17:35 +0530

    @ player added with basic movments and fullscreen(with alt + enter)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,62 @@
+use tcod::colors::*;
+use tcod::console::*;
+use tcod::input::{Key, KeyCode::*};
+
+const SCREEN_WIDTH: i32 = 80;
+const SCREEN_HEIGHT: i32 = 50;
+
+const LIMIT_FPS: i32 = 20;
+
+struct Tcod{
+    root: Root,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let root = Root::initializer()
+        .font("arial10x10.png", FontLayout::Tcod)
+        .font_type(FontType::Greyscale)
+        .size(SCREEN_WIDTH, SCREEN_HEIGHT)
+        .title("idk game i guess")
+        .init();
+
+    let mut tcod = Tcod {root};
+
+    tcod::system::set_fps(LIMIT_FPS);
+
+    let mut player_x = SCREEN_WIDTH/2;
+    let mut player_y = SCREEN_HEIGHT/2;
+
+    while !tcod.root.window_closed(){
+        tcod.root.set_default_foreground(WHITE);
+        tcod.root.clear();
+        tcod.root.put_char(player_x, player_y, '@', BackgroundFlag::None);
+        tcod.root.flush();
+        handle_keys(&mut tcod, &mut player_x, &mut player_y);
+        tcod.root.wait_for_keypress(true);
+    }
+}
+
+fn handle_keys(tcod: &mut Tcod, player_x: &mut i32, player_y: &mut i32) -> bool{
+    let key = tcod.root.wait_for_keypress(true);
+
+    match key {
+        Key {code: Up, ..} => *player_y -= 1,
+        Key {code: Down, ..} => *player_y += 1,
+        Key {code: Left, ..} => *player_x -= 1,
+        Key {code: Right, ..} => *player_x += 1,
+
+        Key { printable: 'w', .. } => *player_y -= 1,
+        Key { printable: 's', .. } => *player_y += 1,
+        Key { printable: 'a', .. } => *player_x -= 1,
+        Key { printable: 'd', .. } => *player_x += 1,
+
+        Key {code: Enter, alt: true, ..} => {
+            let fullscreen = tcod.root.is_fullscreen();
+            tcod.root.set_fullscreen(!fullscreen);
+        }
+
+        _ => {}
+    }
+
+    false
 }
