fef6615cf5b40853b27dea161e3d46421b81da15
hakabol <habelcijuanu@gmail.com>
2026-05-18 14:15:33 +0530

    random dungeon generation added

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use tcod::colors::*;
 use tcod::console::*;
 use tcod::input::{Key, KeyCode::*};
 
+use rand::RngExt;
+
 use std::cmp;
 
 const SCREEN_WIDTH: i32 = 80;
@@ -16,6 +18,10 @@ const X_SUB: i32 = SCREEN_WIDTH - MAP_WIDTH;
 
 const COLOR_DARK_WALL: Color = Color {r: 0, g: 0, b: 100};
 const COLOR_DARK_GROUND: Color = Color {r: 50, g: 50, b: 150};
+
+const ROOM_MAX_SIZE: i32 = 10;
+const ROOM_MIN_SIZE: i32 = 6;
+const MAX_ROOMS: i32 = 30;
 
 const LIMIT_FPS: i32 = 20;
 
@@ -89,6 +95,7 @@ struct Tcod{
     con: Offscreen,
 }
 
+#[derive(Clone, Copy, Debug)]
 struct Rect{
     x1: i32,
     y1: i32,
@@ -104,6 +111,20 @@ impl Rect{
             x2: x + w,
             y2: y + h,
         }
+    }
+
+    pub fn center(&self) -> (i32, i32){
+        let center_x = (self.x1 + self.x2) / 2;
+        let center_y = (self.y1 + self.y2) / 2;
+
+        (center_x, center_y)
+    }
+
+    pub fn intersects_with(&self, other: &Rect) -> bool{
+        (self.x1 <= other.x2) &&
+        (self.x2 >= other.x1) &&
+        (self.y1 <= other.y2) &&
+        (self.y2 <= other.y1)
     }
 }
 
@@ -122,19 +143,67 @@ fn main() {
     tcod::system::set_fps(LIMIT_FPS);
 
     let enemy = Character::new(SCREEN_WIDTH/2 - 5, SCREEN_HEIGHT/2 - 5, '#', WHITE);
-    let player = Character::new(25, 23, '@', WHITE);
+    let mut player = Character::new(25, 23, '@', WHITE);
 
-    let mut objects = vec![player, enemy];
 
     let mut map = make_map();
-    
+    /*
     let room1 = Rect::new(20, 15, 10, 15);
     let room2 = Rect::new(50, 15, 10, 15);
+    let room3 = Rect::new(35, 0, 10, 15);
 
     create_room(room1, &mut map);
     create_room(room2, &mut map);
+    create_room(room3, &mut map);
     create_h_tunnel(30, 50, 23, &mut map);
+    create_v_tunnel(23, 15, 40, &mut map);
+    */
+
+    let mut rooms: Vec<Rect> = vec![];
+
+    for _ in 0..MAX_ROOMS{
+        let w = rand::rng().random_range(ROOM_MIN_SIZE..(ROOM_MAX_SIZE + 1));
+        let h = rand::rng().random_range(ROOM_MIN_SIZE..(ROOM_MAX_SIZE + 1));
+
+        let x = rand::rng().random_range(0..(MAP_WIDTH - w));
+        let y = rand::rng().random_range(0..(MAP_HEIGHT - h));
+
+        let room = Rect::new(x, y, w, h);
+
+        let failed = rooms.iter().any(|other_room| room.intersects_with(other_room));
+
+        if !failed{
+            create_room(room, &mut map);
+
+            let (new_x, new_y) = room.center();
+
+            if rooms.is_empty(){
+                player.x = new_x;
+                player.y = new_y;
+            }
+            
+            else {
+                let (prev_x, prev_y) = rooms[rooms.len() - 1].center();
+
+                if rand::rng().random(){
+                    create_h_tunnel(prev_x, new_x, prev_y, &mut map);
+                    create_v_tunnel(prev_y, new_y, new_x, &mut map);
+                }
+
+                else{
+                    create_v_tunnel(prev_y, new_y, prev_x, &mut map);
+                    create_h_tunnel(prev_x, new_x, new_y, &mut map);
+                }
+            }
+
+            rooms.push(room);
+
+        }
+
+    }
     
+    let mut objects = vec![player, enemy];
+
     let game = Game { map: map };
 
     while !tcod.root.window_closed(){
@@ -179,7 +248,7 @@ fn handle_keys(tcod: &mut Tcod, object: &mut Character, game: &Game, objects: &V
 }
 
 fn make_map() -> Map{
-    let mut map = vec![vec![Tile::wall(); MAP_HEIGHT as usize]; MAP_WIDTH as usize];
+    let map = vec![vec![Tile::wall(); MAP_HEIGHT as usize]; MAP_WIDTH as usize];
 
     map
 }
@@ -223,6 +292,12 @@ fn create_room(room: Rect, map: &mut Map){
 
 fn create_h_tunnel(x1: i32, x2: i32, y: i32, map: &mut Map){
     for x in cmp::min(x1, x2)..=cmp::max(x1, x2){
+        map[x as usize][y as usize] = Tile::empty();
+    }
+}
+
+fn create_v_tunnel(y1: i32, y2: i32, x: i32, map: &mut Map){
+    for y in cmp::min(y1, y2)..=cmp::max(y1, y2){
         map[x as usize][y as usize] = Tile::empty();
     }
 }
